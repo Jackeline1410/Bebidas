ff2c2af81ed3a7cad95bb7fbf05684e055afa846
Jackeline <ing.jacke@gmail.com>
2021-03-23 08:44:59 -0500

    Obteniendo las recetas

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
-import CategoriasProvider from './context/CategoriasContex';
+import CategoriasProvider from './context/CategoriasContext';
+import RecetasProvider from './context/RecetasContext';
 
 function App() {
   return (
     <CategoriasProvider>
+      <RecetasProvider>
       <Header />
 
     <div className="container mt-5">
@@ -14,7 +16,7 @@ function App() {
         <Formulario />
       </div>
     </div>
-
+      </RecetasProvider>
     </CategoriasProvider>
   );
 }
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
-import { CategoriasContext } from '../context/CategoriasContex';
+import { CategoriasContext } from '../context/CategoriasContext';
+import { RecetasContext } from '../context/RecetasContext';
 
 const Formulario = () => {
 
@@ -9,6 +10,7 @@ const Formulario = () => {
     });
 
     const { categorias } = useContext(CategoriasContext);
+    const { buscarRecetas, guardarConsultar } = useContext(RecetasContext);
 
     // función para leer los contenidos
     const obtenerDatosReceta = e => {
@@ -20,7 +22,12 @@ const Formulario = () => {
 
     return ( 
         <form
-            className="col-md-12"
+            className="col-12"
+            onSubmit={ e => {
+                e.preventDefault();
+                buscarRecetas(busqueda);
+                guardarConsultar(true);
+            }}
         >
         <fieldset className="text-center">
             <legend>Busca bebidas por Categoría o Ingrediente</legend>
